6c0cca71de40ef9549eaf832a15f319ab27eaacd
stea1th <38189826+stea1th@users.noreply.github.com>
2024-10-01 00:48:25 +0200

    Update CalculationProcessor.java

--- src/main/java/org/example/CalculationProcessor.java
+++ src/main/java/org/example/CalculationProcessor.java
@@ -5,37 +5,55 @@ public final class CalculationProcessor {
     private CalculationProcessor() {
     }
 
+    public static void checkMenuChoice(CalculatorModel calculatorModel) {
+        calculatorModel.setMenuCheck(true);
+        calculateResult(calculatorModel);
+        calculatorModel.setMenuCheck(false);
+    }
+
     public static CalculatorModel calculateResult(CalculatorModel calculatorModel) {
         return switch (calculatorModel.getChoice()) {
-            case 0 -> finishCalculator(calculatorModel);
+            case 0 -> finishCalculator();
             case 1 -> addition(calculatorModel);
             case 2 -> subtraktion(calculatorModel);
             case 3 -> multiplikation(calculatorModel);
             case 4 -> division(calculatorModel);
             case 5 -> modulo(calculatorModel);
-            default -> retryCalculation(calculatorModel);
+            default -> retryCalculation();
         };
     }
 
     private static CalculatorModel addition(CalculatorModel calculatorModel) {
+        if (calculatorModel.isMenuCheck()) {
+            return calculatorModel;
+        }
         var result = calculatorModel.getFirstNumber() + calculatorModel.getSecondNumber();
         calculatorModel.setResult(result);
         return calculatorModel;
     }
 
     private static CalculatorModel subtraktion(CalculatorModel calculatorModel) {
+        if (calculatorModel.isMenuCheck()) {
+            return calculatorModel;
+        }
         var result = calculatorModel.getFirstNumber() - calculatorModel.getSecondNumber();
         calculatorModel.setResult(result);
         return calculatorModel;
     }
 
     private static CalculatorModel multiplikation(CalculatorModel calculatorModel) {
+        if (calculatorModel.isMenuCheck()) {
+            return calculatorModel;
+        }
         var result = calculatorModel.getFirstNumber() * calculatorModel.getSecondNumber();
         calculatorModel.setResult(result);
         return calculatorModel;
     }
 
     private static CalculatorModel division(CalculatorModel calculatorModel) throws ArithmeticException {
+        if (calculatorModel.isMenuCheck()) {
+            return calculatorModel;
+        }
         var firstNumber = calculatorModel.getFirstNumber();
         if (firstNumber == 0) {
             throw new CalculatorException("Teilung durch Null ist nicht erlaubt", false);
@@ -46,6 +64,9 @@ public final class CalculationProcessor {
     }
 
     private static CalculatorModel modulo(CalculatorModel calculatorModel) throws ArithmeticException {
+        if (calculatorModel.isMenuCheck()) {
+            return calculatorModel;
+        }
         var secondNumber = calculatorModel.getSecondNumber();
         if (secondNumber == 0) {
             throw new CalculatorException("Modulo durch Null ist nicht möglich", false);
@@ -55,14 +76,11 @@ public final class CalculationProcessor {
         return calculatorModel;
     }
 
-    private static CalculatorModel finishCalculator(CalculatorModel calculatorModel) {
-        System.out.println("Taschenrechner beendet.");
-        calculatorModel.setCancelCalculation(true);
-        return calculatorModel;
+    private static CalculatorModel finishCalculator() {
+        throw new CalculatorException("Taschenrechner beendet.", true);
     }
 
-    private static CalculatorModel retryCalculation(CalculatorModel calculatorModel) {
-        System.out.println("Ungültige Auswahl. Bitte wählen Sie eine Zahl zwischen 0 und 6.");
-        return calculatorModel;
+    private static CalculatorModel retryCalculation() {
+        throw new CalculatorException("Ungültige Auswahl. Bitte wählen Sie eine Zahl zwischen 0 und 6.", false);
     }
 }
